a50c52230ebdc993836a1e2f1b30f1559c3ab9c2
Susi Indah Lestari <38420658+susiindahlestari244@users.noreply.github.com>
2018-05-14 09:42:41 +0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,23 +1,4 @@
 #include<iostream>
-#include<conio.h>
+
 using namespace std;
 
-int main (){
-    int a, b, c;
-
-    cout << "menentukan nilai terbesar 3 buah input \n";
-    cout << "masukkan nilai 1 : "; cin >> a;
-    cout << "masukkan nilai 2 : "; cin >> b;
-    cout << "masukkan nilai 3 : "; cin >> c;
-
-    if ( (a > b )&& (a > c )){
-
-        cout << "nilai terbesar   : " << a << endl;
-
-    } else if ((b > a) && (b > c ))
-        cout << "nilai terbesar   : " << b << endl;
-    else
-        cout << "nilai terbesar   : " << c << endl;
-
-    getch();
-}
